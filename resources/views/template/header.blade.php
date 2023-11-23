d1de372c1a53fe1cb2f41902b75a71014ea6c42c
Ival Shamkya <ivalshamkya@gmail.com>
2023-11-23 20:51:29 +0800

    feat(dashboard): update dashboard template

--- resources/views/template/header.blade.php
+++ resources/views/template/header.blade.php
@@ -5,5 +5,5 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Antrian</title>
-    @vite('resources/css/app.css')
+    @vite(['resources/css/app.css','resources/js/app.js'])
 </head>
--- resources/views/template/template.blade.php
+++ resources/views/template/template.blade.php
@@ -1,41 +1,117 @@
 @extends('template.header')
+
 <body class="antialiased">
-    <nav class="fixed top-0 left-0 right-0 bg-[#c7e4e8] text-white pl-72">
-        <div class="w-screen mx-auto flex items-center justify-between">
-            <div class="flex items-center w-[577px] justify-between">
-                <button class="text-3xl" id="sidebar-toggle">
-                    <ion-icon name="menu"></ion-icon>
-                </button>
-            </div>
-            <div class="flex justify-between">
-                <button class="bg-purple-200 px-[36px] py-[10px] rounded-[47px]">
-                    <p class="text-purple-700">Sign in</p>
-                </button>
-                <button class="bg-purple-600 px-[36px] py-[10px] rounded-[47px]">
-                    <p class="text-white">Sign Up</p>
-                </button>
+    <nav class="fixed top-0 z-50 w-full bg-[#c7e4e8]">
+        <div class="px-3 py-3 lg:px-5 lg:pl-3">
+            <div class="flex items-center justify-between">
+                <div class="flex items-center justify-start rtl:justify-end">
+                    <button data-drawer-target="logo-sidebar" data-drawer-toggle="logo-sidebar"
+                        aria-controls="logo-sidebar" type="button"
+                        class="inline-flex items-center p-2 text-sm text-gray-900 rounded-lg sm:hidden hover:bg-white focus:outline-none focus:ring-2 focus:ring-gray-200">
+                        <span class="sr-only">Open sidebar</span>
+                        <svg class="w-6 h-6" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20"
+                            xmlns="http://www.w3.org/2000/svg">
+                            <path clip-rule="evenodd" fill-rule="evenodd"
+                                d="M2 4.75A.75.75 0 012.75 4h14.5a.75.75 0 010 1.5H2.75A.75.75 0 012 4.75zm0 10.5a.75.75 0 01.75-.75h7.5a.75.75 0 010 1.5h-7.5a.75.75 0 01-.75-.75zM2 10a.75.75 0 01.75-.75h14.5a.75.75 0 010 1.5H2.75A.75.75 0 012 10z">
+                            </path>
+                        </svg>
+                    </button>
+                    <a href="{{ route('dashboard') }}" class="flex ms-2 md:me-24">
+                        <span
+                            class="self-center text-xl font-semibold sm:text-2xl whitespace-nowrap dark:text-white">Antrian</span>
+                    </a>
+                </div>
+                <div class="flex items-center">
+                    <div class="flex items-center ms-3">
+                        <div>
+                            <button type="button"
+                                class="flex text-sm bg-gray-800 rounded-full focus:ring-4 focus:ring-gray-300 dark:focus:ring-gray-600"
+                                aria-expanded="false" data-dropdown-toggle="dropdown-user">
+                                <span class="sr-only">Open user menu</span>
+                                <img class="w-8 h-8 rounded-full"
+                                    src="https://flowbite.com/docs/images/people/profile-picture-5.jpg"
+                                    alt="user photo">
+                            </button>
+                        </div>
+                        <div class="z-50 hidden my-4 text-base list-none bg-white divide-y divide-gray-100 rounded shadow dark:bg-gray-700 dark:divide-gray-600"
+                            id="dropdown-user">
+                            <div class="px-4 py-3" role="none">
+                                <p class="text-sm text-gray-900 dark:text-white" role="none">
+                                    Neil Sims
+                                </p>
+                                <p class="text-sm font-medium text-gray-900 truncate dark:text-gray-300" role="none">
+                                    neil.sims@flowbite.com
+                                </p>
+                            </div>
+                            <ul class="py-1" role="none">
+                                <li>
+                                    <a href="#" class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100"
+                                        role="menuitem">Dashboard</a>
+                                </li>
+                                <li>
+                                    <a href="#" class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100"
+                                        role="menuitem">Settings</a>
+                                </li>
+                                <li>
+                                    <a href="#" class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100"
+                                        role="menuitem">Earnings</a>
+                                </li>
+                                <li>
+                                    <a href="#" class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100"
+                                        role="menuitem">Sign out</a>
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
             </div>
         </div>
     </nav>
-    <aside class="w-72 h-screen bg-[#62aac2] p-5 fixed top-0 z-10 transition-transform transform -translate-x-full" id="sidebar"> <!-- Added 'fixed' and adjusted top position -->
-        <h1 class="text-white text-3xl text-center mb-5">Antrian</h1>
-        <ul class="gap-6 space-y-5">
-            <li class="group"><a href="" class="w-full flex items-center gap-2 text-white font-semibold text-xl"><ion-icon name="home"></ion-icon>Beranda</a></li>
-            <li class="group"><a href="" class="w-full flex items-center gap-2 text-white font-semibold text-xl"><ion-icon name="volume-high"></ion-icon>Antrian</a></li>
-            <li class="group"><a href="" class="w-full flex items-center gap-2 text-white font-semibold text-xl"><ion-icon name="document-outline"></ion-icon>Rekap Data</a></li>
-        </ul>
+
+    <aside id="logo-sidebar"
+        class="fixed top-0 left-0 z-40 w-64 h-screen pt-20 transition-transform -translate-x-full bg-[#62aac2] border-r border-gray-200 sm:translate-x-0 dark:bg-gray-800 dark:border-gray-700"
+        aria-label="Sidebar">
+        <div class="h-full px-3 pb-4 overflow-y-auto ">
+            <ul class="space-y-2 font-medium">
+                <li>
+                    <a href="#"
+                        class="flex items-center p-1.5 rounded-lg text-white hover:bg-gray-100 group transition-all ease-in-out duration-300">
+                        <ion-icon name="home"
+                            class="p-2 border border-zinc-300 bg-white text-zinc-900 rounded-lg group-hover:bg-zinc-800 group-hover:text-white"></ion-icon>
+                        <span class="ms-3 group-hover:text-gray-900">Beranda</span>
+                    </a>
+                </li>
+                <li>
+                    <a href="#"
+                        class="flex items-center p-1.5 rounded-lg text-white hover:bg-gray-100 group transition-all ease-in-out duration-300">
+                        <ion-icon name="volume-high"
+                            class="p-2 border border-zinc-300 bg-white text-zinc-900 rounded-lg group-hover:bg-zinc-800 group-hover:text-white"></ion-icon>
+                        <span class="ms-3 group-hover:text-gray-900">Antrian</span>
+                    </a>
+                </li>
+                <li>
+                    <a href="#"
+                        class="flex items-center p-1.5 rounded-lg text-white hover:bg-gray-100 group transition-all ease-in-out duration-300">
+                        <ion-icon name="document"
+                            class="p-2 border border-zinc-300 bg-white text-zinc-900 rounded-lg group-hover:bg-zinc-800 group-hover:text-white"></ion-icon>
+                        <span class="ms-3 group-hover:text-gray-900">Rekap Data</span>
+                    </a>
+                </li>
+            </ul>
+        </div>
     </aside>
-    <main class="ml-72 flex gap-5 mt-[100px]"> <!-- Adjusted margin-top to account for the fixed navbar -->
-        @yield('content')
+
+    <div class="p-4 sm:ml-64">
+        <div class="p-4 border-2 border-gray-200 border-dashed rounded-lg dark:border-gray-700 mt-14">
+            @yield('content')
+        </div>
         @extends('template.footer')
-    </main>
+        </main>
+    </div>
+
+
     <script type="module" src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.esm.js"></script>
     <script nomodule src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.js"></script>
-
-    <script>
-        document.getElementById('sidebar-toggle').addEventListener('click', function () {
-            document.getElementById('sidebar').classList.toggle('-translate-x-full');
-        });
-    </script>
 </body>
+
 </html>
